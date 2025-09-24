892e6e475858f2efe4e06b3efa117300a9fc6f5d
obinna <110184163+onyenagubo9@users.noreply.github.com>
2025-09-24 12:31:05 +0100

    Update page.jsx

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -8,7 +8,7 @@ export default function RegisterPage() {
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
   const router = useRouter();
-
+ const API_URL = process.env.NEXT_PUBLIC_API_URL;
   const handleSubmit = async (e) => {
     e.preventDefault();
 
